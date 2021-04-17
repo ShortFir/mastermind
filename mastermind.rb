7feb0caeb4ff29e69368aaab3246344f07d9f18a
ShortFir <shortfir@protonmail.com>
2021-04-17 16:39:35 +1000

    Initial class layout.

---
+++ mastermind.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class CodeMaker
+end
+
+class CodeBreaker
+end
+
+class GameBoard
+end
+
+# Make other classes protected? do it above initialize?
+class Play
+  def initialize
+  end
+
+  def game
+  end
+end
+
+Play.new.game
